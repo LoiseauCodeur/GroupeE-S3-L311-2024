61e691ae8511a9aa315c7208a09c2d296c27be79
Clem Lons <clemaxel16@gmail.com>
2024-10-20 18:41:16 +0200

    add all

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,38 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    let is_run    = "true";
+    let is_run    = true;
 
     init();
 
     function init(){
-       document.querySelector('.button').addEventListener('clic', (event) => 
+       document.querySelector('.button').addEventListener('click', (event) => {
             is_run = !is_run
-            ((' ' + document.querySelector('.button').className + ' ').indexOf('pause') > -1) ? dcument.querySelector('.button').setAttribute('class', document.querySelector('.button').getAttribute('class').replace(' pause', '')) : document.querySelector('.button').setAttribute('class', document.querySelector('.button').getAttribute('class')+' pause');
-        })
-
-        setTimeInterval(function(){
-            if(is_run){
-                let oDate = new Date();
-                document.querySelector('#hours').innerHTML   =  adjustTimer(oDate.getHours());
-                document.querySelector('#minutes').innerHTML =  adjustTimer(oDate.getMinutes());
-                document.querySelector('#seconds').innerHTML =  adjustTimer( oDate.getSeconds());
-
-                document.querySelector('body').style.background = randomHexColor(document.querySelector('#hours').innerHTML, document.querySelector('#minutes').innerHTML, document.querySelector('#seconds').innerHTML);
-                
+            if (document.querySelector('.button').classList.contains('pause')) {
+                document.querySelector('.button').classList.remove('pause');
+            } else {
+                document.querySelector('.button').classList.add('pause');
             }
-        }, 1000;
+        });
+    
+
+        setInterval(function(){
+            if (is_run) {
+                let oDate = new Date();
+                document.querySelector('#hours').innerHTML   = adjustTimer(oDate.getHours());
+                document.querySelector('#minutes').innerHTML = adjustTimer(oDate.getMinutes());
+                document.querySelector('#seconds').innerHTML = adjustTimer(oDate.getSeconds());
+
+                document.querySelector('body').style.background = randomHexColor(
+                    oDate.getHours(), oDate.getMinutes(), oDate.getSeconds()
+                );
+            }
+        }, 1000);
     }
 
-    function adjustTimer(timer){
-        (timer < 10 ? '0'+timer : timer);
+    function adjustTimer(timer) {
+        return (timer < 10 ? '0' + timer : timer);
     }
 
-    function randomHexColor(x, y){
+    function randomHexColor(x, y, z){
         return "rgb(" + Math.floor(x/100 * 256) + "," + Math.floor(y/100 * 256) + "," + Math.floor(z/100 * 256) + ")";
     }
 });
